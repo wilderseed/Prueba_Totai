5bdafde7f28b640c82fe82b5082adb04a9b9417b
sistema <sistema@DESKTOP-IO1RV1U>
2017-10-11 08:39:36 -0400

    Agregar archivos de proyecto.

---
+++ Seed_v1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Seed_v1.MvcApplication" Language="C#" %>
---
+++ Seed_v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Seed_v1.Startup))]
+namespace Seed_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
